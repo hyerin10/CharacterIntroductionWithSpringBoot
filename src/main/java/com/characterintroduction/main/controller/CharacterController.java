cff2ecc151be119cbc097ee51e82266cc1040c57
hyerin10 <dev.hr.kim@gmail.com>
2023-09-03 03:33:41 +0900

    (BE) swagger api documentation

--- src/main/java/com/characterintroduction/main/controller/CharacterController.java
+++ src/main/java/com/characterintroduction/main/controller/CharacterController.java
@@ -17,34 +17,44 @@ import com.characterintroduction.main.dto.MemoDTO;
 import com.characterintroduction.main.service.CharacterService;
 import com.characterintroduction.main.service.MemoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/api")
+@Api(tags = "Character Controller")
 public class CharacterController {
 	@Autowired
 	CharacterService characterService;
 	
 	@PostMapping("/character")
-	public CharacterDTO insertMemo(@RequestBody CharacterDTO character) throws Exception {
+	@ApiOperation("Insert a new character")
+	public CharacterDTO insertCharacter(@RequestBody CharacterDTO character) throws Exception {
 		return characterService.insertCharacter(character);
 	}
 	
 	@GetMapping("/characters")
-	public List<CharacterDTO> getAllMemos() throws Exception {
+	@ApiOperation("Get list of all characters")
+	public List<CharacterDTO> getAllCharacters() throws Exception {
 		return characterService.getAllCharacters();
 	}
 	
 	@GetMapping("/character/{characterNum}")
-	public CharacterDTO getMemoByMemoNum(@PathVariable String characterNum) throws Exception {
+	@ApiOperation("Get character by character number")
+	public CharacterDTO getCharacterByCharacterNum(@PathVariable @ApiParam(value = "Character number", example = "1") String characterNum) throws Exception {
 		return characterService.getCharacterByCharacterNum(characterNum);
 	}
 	
 	@PutMapping("/character/{characterNum}")
-	public void updateMemo(@PathVariable String characterNum, @RequestBody CharacterDTO character) throws Exception {
+	@ApiOperation("Update character information")
+	public void updateCharacter(@PathVariable @ApiParam(value = "Character number", example = "1") String characterNum, @RequestBody @ApiParam(value = "Updated character information") CharacterDTO character) throws Exception {
 		characterService.updateCharacter(characterNum, character);
 	}
 	
 	@DeleteMapping("/character/{characterNum}")
-	public void deleteMemo(@PathVariable String characterNum) throws Exception {
+	@ApiOperation("Delete character by character number")
+	public void deleteCharacter(@PathVariable @ApiParam(value = "Character number", example = "1")  String characterNum) throws Exception {
 		characterService.deleteCharacter(characterNum);
 	}
 }
--- src/main/java/com/characterintroduction/main/controller/MemoController.java
+++ src/main/java/com/characterintroduction/main/controller/MemoController.java
@@ -15,35 +15,45 @@ import org.springframework.web.bind.annotation.RestController;
 import com.characterintroduction.main.dto.MemoDTO;
 import com.characterintroduction.main.service.MemoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/api")
+@Api(tags = "Memo Controller")
 public class MemoController {
 	@Autowired
 	MemoService memoService;
 	
 	@PostMapping("/memo")
-	public MemoDTO insertMemo(@RequestBody MemoDTO memo) throws Exception {
+	@ApiOperation("Insert a new memo")
+	public MemoDTO insertMemo(@RequestBody @ApiParam(value = "Memo information to insert") MemoDTO memo) throws Exception {
 		memoService.insertMemo(memo);
 		return memo;
 	}
 	
 	@GetMapping("/memos")
+	@ApiOperation("Get list of all memos")
 	public List<MemoDTO> getAllMemos() throws Exception {
 		return memoService.getAllMemos();
 	}
 	
 	@GetMapping("/memo/{memoNum}")
-	public MemoDTO getMemoByMemoNum(@PathVariable String memoNum) throws Exception {
+	@ApiOperation("Get memo by memo number")
+	public MemoDTO getMemoByMemoNum(@PathVariable @ApiParam(value = "Memo number", example = "1") String memoNum) throws Exception {
 		return memoService.getMemoByMemoNum(memoNum);
 	}
 	
 	@PutMapping("/memo/{memoNum}")
-	public void updateMemo(@PathVariable String memoNum, @RequestBody MemoDTO memo) throws Exception {
+	@ApiOperation("Update memo information")
+	public void updateMemo(@PathVariable @ApiParam(value = "Memo number", example = "1") String memoNum, @RequestBody @ApiParam(value = "Updated memo information") MemoDTO memo) throws Exception {
 		memoService.updateMemo(memoNum, memo);
 	}
 	
 	@DeleteMapping("/memo/{memoNum}")
-	public void deleteMemo(@PathVariable String memoNum) throws Exception {
+	@ApiOperation("Delete memo by memo number")
+	public void deleteMemo(@PathVariable @ApiParam(value = "Memo number", example = "1") String memoNum) throws Exception {
 		memoService.deleteMemo(memoNum);
 	}
 }
